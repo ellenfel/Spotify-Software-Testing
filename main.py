abd7df7ae73788952dd05661c6bdfe8f2ff833fd
ellenfel <Omerfbayir@protonmail.com>
2022-06-26 09:49:07 +0300

    Genre tested

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from logInOut import *
 from Play import *
+from genre import *
+
 
 # Tests LogIn-Out
 LogInOut()
 
 # Tests Search & Play utilities
 play()
+
+# Recomanded Genres Located and a mix Played
+testGenre()
 
 #LogOut()
 
