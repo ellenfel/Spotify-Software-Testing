b78d172d2a2eec1987eddc55b76af1fc53c726ea
ellenfel <Omerfbayir@protonmail.com>
2022-06-27 23:30:31 +0300

    recurrsive error handler

--- templates4EH.py
+++ templates4EH.py
@@ -10,21 +10,52 @@
 #finally:
 
 from logInOut import *
+from Play import *
+from genre import *
+
 i = 0
+j = 0
+k = 0
 
 def recurrsiveEH(i):
     try:
         LogInOut()
+        
 
     except:
         i += 1
-        print(i)
+        print("#Log(in-out) failures: ",i)
         recurrsiveEH(i)
 
-    else:
-        return i
 
+    else:
+        def recurrsiveEH1(j):
+            try:
+                play()
+                
+
+            except:
+                j += 1
+                print("#Play failures: ",j)
+                recurrsiveEH1(j)
+
+            else:
+                def recurrsiveEH2(k):
+                    try:
+                        testGenre()
+                        
+
+                    except:
+                        k += 1
+                        print("#testGenre failures: "k)
+                        recurrsiveEH2(k)
+
+                    else:
+                        return i, j, k
+                
 recurrsiveEH(i)
 
-if(i!=0):
-    print("number of exceptions during tests = ",i)
+test1, test2, test3 = i,j,k
+print("number of test failures happend during func 1 testing is: ",test1)
+print("number of test failures happend during func 2 testing is: ",test2)
+print("number of test failures happend during func 3 testing is: ",test3)
