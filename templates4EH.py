e75a8b6a0047fe85683877629184dd7ebeedf568
ellenfel <Omerfbayir@protonmail.com>
2022-06-27 19:16:33 +0300

    Created a template for error handling

---
+++ templates4EH.py
@@ -0,0 +1,23 @@
+#try:
+#   function()
+
+#except:
+
+#except:
+
+#else:
+
+#finally:
+
+from logInOut import *
+i=0
+
+try:
+    LogInOut()
+
+except:
+    i += 1
+    LogInOut()
+
+else:
+    print(i)
