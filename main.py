2b177a449fb3dfa631db2e2d795912f39f2521f3
ellenfel <Omerfbayir@protonmail.com>
2022-07-12 08:19:44 +0300

    Created block.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from blocks import *
 from logInOut import *
 from Play import *
 from genre import *
@@ -9,6 +10,20 @@ j = 0
 k = 0
 l = 0
 m = 0
+n = 0
+
+#Block Funcstinality
+def recursiveEH5():
+
+    global n
+
+    try:
+        block()
+
+    except:
+        n += 1
+        print("#Block failures: ",n)
+        recursiveEH5()
 
 #Settings & recepiets Funcstinality
 def recursiveEH4():
@@ -22,6 +37,9 @@ def recursiveEH4():
         m += 1
         print("#Settings failures: ",m)
         recursiveEH4()
+
+    else:
+        recursiveEH5()
 
 #follow -unfollow functinality
 def recursiveEH3():
